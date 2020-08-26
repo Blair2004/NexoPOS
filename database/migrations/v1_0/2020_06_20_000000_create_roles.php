f0d5b92dc63a69374c6030c6f765cec3978cde94
blair2004 <carlosjohnsonluv2004@gmail.com>
2020-08-26 09:04:19 +0100

    Update - fixed:  new way to add permissions to roles

--- database/migrations/v1_0/2020_06_20_000000_create_roles.php
+++ database/migrations/v1_0/2020_06_20_000000_create_roles.php
@@ -32,31 +32,7 @@ class CreateRoles extends Migration
         $storeAdmin->locked         =   true;
         $storeAdmin->description    =   __( 'Has a control over an entire store of NexoPOS' );
         $storeAdmin->save();
-        
-        /**
-         * store administrator role
-         */
-        $storeCashier               =   new Role;
-        $storeCashier->name         =   __( 'Store Cashier' );
-        $storeCashier->namespace    =   'nexopos.store.cashier';
-        $storeCashier->locked       =   true;
-        $storeCashier->description  =   __( 'Has a control over the sale process' );
-        $storeCashier->save();
-
-        /**
-         * store administrator role
-         */
-        $driver                     =   new Role;
-        $driver->name               =   __( 'Vehicule Driver' );
-        $driver->namespace          =   'nexopos.store.driver';
-        $driver->locked             =   true;
-        $driver->description        =   __( 'Does the delivery' );
-        $driver->save();
-
-        /**
-         * assigning permissions to roles
-         */
-        $storeAdmin         =   Role::namespace( 'nexopos.store.administrator' );
+        $storeAdmin->addPermissions( Permission::includes( '.profile' )->get()->map( fn( $permission ) => $permission->namespace ) );
         $storeAdmin->addPermissions( Permission::includes( '.categories' )->get()->map( fn( $permission ) => $permission->namespace ) );
         $storeAdmin->addPermissions( Permission::includes( '.products' )->get()->map( fn( $permission ) => $permission->namespace ) );
         $storeAdmin->addPermissions( Permission::includes( '.expenses' )->get()->map( fn( $permission ) => $permission->namespace ) );
@@ -68,7 +44,30 @@ class CreateRoles extends Migration
         $storeAdmin->addPermissions( Permission::includes( '.stores' )->get()->map( fn( $permission ) => $permission->namespace ) );
         $storeAdmin->addPermissions( Permission::includes( '.taxes' )->get()->map( fn( $permission ) => $permission->namespace ) );
         $storeAdmin->addPermissions( Permission::includes( '.trucks' )->get()->map( fn( $permission ) => $permission->namespace ) );
-    }
+        $storeAdmin->addPermissions( Permission::includes( '.profile' )->get()->map( fn( $permission ) => $permission->namespace ) );
+
+        
+        /**
+         * store administrator role
+         */
+        $storeCashier               =   new Role;
+        $storeCashier->name         =   __( 'Store Cashier' );
+        $storeCashier->namespace    =   'nexopos.store.cashier';
+        $storeCashier->locked       =   true;
+        $storeCashier->description  =   __( 'Has a control over the sale process' );
+        $storeCashier->save();
+        $storeCashier->addPermissions( Permission::includes( '.profile' )->get()->map( fn( $permission ) => $permission->namespace ) );
+
+        /**
+         * store administrator role
+         */
+        $driver                     =   new Role;
+        $driver->name               =   __( 'Vehicule Driver' );
+        $driver->namespace          =   'nexopos.store.driver';
+        $driver->locked             =   true;
+        $driver->description        =   __( 'Does the delivery' );
+        $driver->save();
+        $driver->addPermissions( Permission::includes( '.profile' )->get()->map( fn( $permission ) => $permission->namespace ) );    }
 
     /**
      * Reverse the migrations.
