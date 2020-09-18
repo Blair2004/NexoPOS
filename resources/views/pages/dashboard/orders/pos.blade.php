d4216209e4ae38874b2f34233c7e061b616ba722
Developper <carlosjohnsonluv2004@gmail.com>
2020-09-18 19:01:58 +0100

    Update - before adding scrolling

--- resources/views/pages/dashboard/orders/pos.blade.php
+++ resources/views/pages/dashboard/orders/pos.blade.php
@@ -1,53 +1,73 @@
-@extends( 'layout.base' )
+    @extends( 'layout.base' )
 
 @section( 'layout.base.body' )
-<div id="pos-container" class="w-full h-full bg-gray-200 flex flex-col">
-    <div id="pos-header" class="h-12 flex-shrink-0">
+@if( true === false )
+<div class="h-full bg-gray-100 flex flex-col">
+    <div class="h-36 bg-blue-200 flex-shrink-0">C</div>
+    <div class="flex flex-auto overflow-hidden bg-red-200">
+        <div class="w-full">
+            <div class="h-full bg-gray-100 flex flex-col">
+                <div class="h-36 bg-blue-200 flex-shrink-0">C</div>
+                <div class="flex overflow-hidden bg-red-200">
+                    <div class="overflow-auto w-full">
+                    <div style="height: 500px" class="flex flex-auto">Hello World</div>
+                    <div style="height: 500px" class="flex flex-auto">Hello World</div>
+                    <div style="height: 500px" class="flex flex-auto">Hello World</div>
+                    <div style="height: 500px" class="flex flex-auto">Hello World</div>
+                    </div>
+                </div>
+                <div class="h-36 bg-blue-200 flex-shrink-0">D</div>
+            </div>
+        </div>
     </div>
-    <div class="p-2 flex-auto flex">
-        <div id="pos-sections" class="-m-2 bg-gray-200 flex flex-auto">
-            <div class="w-1/2 flex pr-1 pl-3 py-2">
-                <div id="pos-cart" class="rounded overflow-x-hidden shadow bg-white flex-auto flex">
-                    <div class="cart-table flex flex-col flex-auto">
+    <div class="h-36 bg-blue-200 flex-shrink-0">D</div>
+</div>
+@endif
+@if( true === true )
+<div class="h-full bg-gray-200 flex flex-col">
+    <div class="h-12 flex-shrink-0"></div>
+    <div class="flex-auto overflow-hidden flex p-2">
+        <div class="flex flex-auto overflow-hidden -m-2">
+            <div class="w-1/2 flex overflow-hidden p-2">
+                <div id="pos-cart" class="rounded shadow bg-white flex-auto flex overflow-hidden">
+                    <div class="cart-table flex flex-auto flex-col overflow-hidden">
                         <div class="w-full text-gray-700 font-semibold flex">
                             <div class="w-4/6 p-2 border border-l-0 border-t-0 border-gray-200 bg-gray-100">{{ __( 'Product' ) }}</div>
                             <div class="w-1/6 p-2 border-b border-t-0 border-gray-200 bg-gray-100">{{ __( 'Qty' ) }}</div>
                             <div class="w-1/6 p-2 border border-r-0 border-t-0 border-gray-200 bg-gray-100">{{ __( 'Total' ) }}</div>
                         </div>
-                        <div class="flex-auto bg-white">
-                            <div class="overflow-y-scroll">
-                                @for( $i = 0; $i < 4; $i++ )
-                                <div class="text-gray-700 flex">
-                                    <div class="w-4/6 p-2 border border-l-0 border-t-0 border-gray-200">
-                                        <h3 class="font-semibold">Some Product</h3>
-                                        <div class="-mx-1 flex">
-                                            <div class="px-1">
-                                                <a class="hover:text-blue-400 cursor-pointer outline-none border-dashed py-1 border-b border-blue-400 text-sm">Price : $25</a>
-                                            </div>
-                                            <div class="px-1"> 
-                                                <a class="hover:text-blue-400 cursor-pointer outline-none border-dashed py-1 border-b border-blue-400 text-sm">Discount 5% : $10</a>
-                                            </div>
+                        <div class="flex flex-auto flex-col overflow-auto">
+                            @for( $i = 0; $i < 20; $i++ )
+                            <div class="text-gray-700 flex">
+                                <div class="w-4/6 p-2 border border-l-0 border-t-0 border-gray-200">
+                                    <h3 class="font-semibold">Some Product</h3>
+                                    <div class="-mx-1 flex">
+                                        <div class="px-1">
+                                            <a class="hover:text-blue-400 cursor-pointer outline-none border-dashed py-1 border-b border-blue-400 text-sm">Price : $25</a>
+                                        </div>
+                                        <div class="px-1"> 
+                                            <a class="hover:text-blue-400 cursor-pointer outline-none border-dashed py-1 border-b border-blue-400 text-sm">Discount 5% : $10</a>
                                         </div>
                                     </div>
-                                    <div class="w-1/6 p-2 border-b border-gray-200 flex items-center justify-center cursor-pointer hover:bg-blue-100">
-                                        <span class="border-b border-dashed border-blue-400 p-2">12</span>
-                                    </div>
-                                    <div class="w-1/6 p-2 border border-r-0 border-t-0 border-gray-200 flex items-center justify-center">$20</div>
                                 </div>
-                                @endfor
+                                <div class="w-1/6 p-2 border-b border-gray-200 flex items-center justify-center cursor-pointer hover:bg-blue-100">
+                                    <span class="border-b border-dashed border-blue-400 p-2">12</span>
+                                </div>
+                                <div class="w-1/6 p-2 border border-r-0 border-t-0 border-gray-200 flex items-center justify-center">$20</div>
                             </div>
+                            @endfor
                         </div>
-                        <div class="h-16 flex border-t border-gray-200">
-                            <div class="flex items-center font-bold text-gray-700 cursor-pointer justify-center hover:bg-green-100 border-r border-gray-200 flex-auto">Pay</div>
-                            <div class="flex items-center font-bold text-gray-700 cursor-pointer justify-center border-r border-gray-200 hover:bg-teal-100 flex-auto">Hold</div>
-                            <div class="flex items-center font-bold text-gray-700 cursor-pointer justify-center border-r border-gray-200 hover:bg-indigo-100 flex-auto">Button</div>
-                            <div class="flex items-center font-bold text-gray-700 cursor-pointer justify-center border-gray-200 hover:bg-red-100 flex-auto">Void</div>
+                        <div class="h-16 flex flex-shrink-0 border-t border-gray-200">
+                            <div class="flex items-center font-bold cursor-pointer justify-center bg-green-500 text-white hover:bg-green-600 border-r border-green-600 flex-auto">Pay</div>
+                            <div class="flex items-center font-bold cursor-pointer justify-center bg-blue-500 text-white border-r hover:bg-blue-600 border-blue-600 flex-auto">Hold</div>
+                            <div class="flex items-center font-bold cursor-pointer justify-center bg-white border-r border-gray-200 hover:bg-indigo-100 flex-auto">Discount</div>
+                            <div class="flex items-center font-bold cursor-pointer justify-center bg-red-500 text-white border-gray-200 hover:bg-red-600 flex-auto">Void</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="w-1/2 flex pr-2 pl-1 py-2">
-                <div id="pos-grid" class="rounded shadow bg-white flex-auto">
+            <div class="w-1/2 p-2 flex overflow-hidden">
+                <div id="pos-grid" class="rounded shadow bg-white overflow-hidden flex-auto">
                     <div id="grid-header" class="p-2 border-b border-gray-200">
                         <div class="border rounded flex border-gray-400 overflow-hidden">
                             <button class="w-10 h-10 bg-gray-200 border-r border-gray-400">S</button>
@@ -55,7 +75,7 @@
                             <input type="text" class="flex-auto outline-none px-2 bg-gray-100">
                         </div>
                     </div>
-                    <div id="grid-breadscrumb" class="p-2 border-b border-gray-200">
+                    <div id="grid-breadscrumb" class="p-2 border-gray-200">
                         <ul class="flex">
                             <li><a href="javascript:void(0)" class="px-3 text-gray-700">Home > </a></li>
                             <li><a href="javascript:void(0)" class="px-3 text-gray-700">Mens > </a></li>
@@ -63,9 +83,11 @@
                             <li><a href="javascript:void(0)" class="px-3 text-gray-700">Sport > </a></li>
                         </ul>
                     </div>
-                    <div id="grid-items" class="overflow-y-auto">
-                        <div>
-                            <div class="h-32 w-32 border border-gray-200"></div>
+                    <div id="grid-items" class="overflow-hidden flex-auto">
+                        <div class="grid grid-cols-6 gap-0 overflow-y-auto">
+                            @for( $i = 0 ; $i < 100 ; $i++ )
+                            <div class="border h-40 border-gray-200">{{ $i }}</div>
+                            @endfor
                         </div>
                     </div>
                 </div>
@@ -73,4 +95,5 @@
         </div>
     </div>
 </div>
+@endif
 @endsection
