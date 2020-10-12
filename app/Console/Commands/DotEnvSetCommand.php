a7f765171805c31529355f82a490a9f144cd4bda
Blair2004 <carlosjohnsonluv2004@gmail.com>
2020-10-12 12:35:16 +0100

    Update

---
+++ app/Console/Commands/DotEnvSetCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Jackiedo\DotenvEditor\Facades\DotenvEditor;
+
+class DotEnvSetCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'env:set {key} {--v=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set an environment value on the .env file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        DotenvEditor::setKey( strtoupper( $this->argument( 'key' ) ), $this->option( 'v' ) );
+        DotEnvEditor::save();
+        $this->info( 'The environment value has been set !' );
+    }
+}
