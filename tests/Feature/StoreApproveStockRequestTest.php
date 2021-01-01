6942d3a0471cd57963db12774ac4eead9b234e02
Blair2004 <carlosjohnsonluv2004@gmail.com>
2021-01-01 15:55:44 +0100

    Update - Fixed : logo typography colors - Added : new test for stock transfer module

--- tests/Feature/StoreApproveStockRequestTest.php
+++ tests/Feature/StoreApproveStockRequestTest.php
@@ -15,7 +15,7 @@ use Modules\NsMultiStore\Models\Store;
 use Modules\NsStockTransfers\Models\StockTransfer;
 use Tests\TestCase;
 
-class StoreApproveStockTransferTest extends TestCase
+class StoreApproveStockRequestTest extends TestCase
 {
     /**
      * @var ProductService
@@ -27,7 +27,7 @@ class StoreApproveStockTransferTest extends TestCase
      *
      * @return void
      */
-    public function testStockTransferApproval()
+    public function testStockRequestApproval()
     {
         Sanctum::actingAs(
             Role::namespace( 'admin' )->users->first(),
@@ -35,49 +35,54 @@ class StoreApproveStockTransferTest extends TestCase
         );
 
         $this->productService       =   app()->make( ProductService::class );
-        $sourceStore                =   Store::first();
-        $destinationStore           =   Store::where( 'id', '<>',  $sourceStore->id )->first();
+        $destinationStore           =   Store::first();
+        $quantityToTransfer         =   3;
+        $sourceStore                =   Store::where( 'id', '<>',  $destinationStore->id )->first();
 
         Store::switchTo( $sourceStore );
 
         $products           =   Product::withStockEnabled()
             ->get()
             ->take(2);
+        
+        $fullProducts       =   $products
+            ->map( function( $product ) use( $quantityToTransfer ) {
+                return [
+                    'name'          =>  $product->name,
+                    'unit_name'     =>  $product->unit_quantities[0]->unit->name,
+                    'quantity'      =>  $quantityToTransfer,
+                    'unit_quantity' =>  $product->unit_quantities[0],
+                    'unit_price'    =>  $product->unit_quantities[0]->sale_price,
+                    'product_id'    =>  $product->id,
+                    'unit_id'       =>  $product->unit_quantities[0]->unit->id,
+                ];
+            });
+
+        Store::switchTo( $destinationStore );
 
         $response       =   $this->withSession( $this->app[ 'session' ]->all() )
             ->json( 'post', 'api/stock-transfers/submit', [
-                'name'          =>  __( 'Test Stock Transfers - ' ) . Str::random(10),
+                'name'          =>  __( 'Test Stock Request - ' ) . Str::random(10),
                 'general'       =>  [
                     'from_store'    =>  $sourceStore->id,
                     'to_store'      =>  $destinationStore->id,
-                    'type'          =>  StockTransfer::TYPE_TRANSFER,
+                    'type'          =>  StockTransfer::TYPE_REQUEST,
                     'status'        =>  StockTransfer::STATUS_PENDING,
                 ],
-                'products'      =>  $products
-                    ->map( function( $product ) {
-                        return [
-                            'name'          =>  $product->name,
-                            'unit_name'     =>  $product->unit_quantities[0]->unit->name,
-                            'quantity'      =>  2,
-                            'unit_quantity' =>  $product->unit_quantities[0],
-                            'unit_price'    =>  $product->unit_quantities[0]->sale_price,
-                            'product_id'    =>  $product->id,
-                            'unit_id'       =>  $product->unit_quantities[0]->unit->id,
-                        ];
-                    })
+                'products'      =>  $fullProducts
             ]);
 
         $response->assertJsonPath( 'status', 'success' );
 
         $responseData   =   json_decode( $response->getContent(), true );
 
+        Store::switchTo( $destinationStore );
+
         /**
          * switch to the destination store in order to approve the stock transfer
          * we need to check if the provided store was existing and what was 
          * the provided stock at that moment.
          */
-        Store::switchTo( $destinationStore );
-
         $products->each( function( Product $product ) {
             $destinationStoreProduct    =   Product::sku( $product->sku )->first();
 
@@ -89,13 +94,15 @@ class StoreApproveStockTransferTest extends TestCase
                 );
             }
         });
+        
+        Store::switchTo( $sourceStore );
 
         $response       =   $this->withSession( $this->app[ 'session' ]->all() )
             ->json( 'get', 'api/stock-transfers/approve/' . $responseData[ 'data' ][ 'transaction' ][ 'id' ] );
 
         Store::switchTo( $destinationStore );
 
-        $products->each( function( $product ) use ( $response ) {
+        $products->each( function( $product ) {
             $destinationStoreProduct    =   Product::sku( $product->sku )->first();
             
             $product->newQuantity      =   0;
@@ -106,14 +113,17 @@ class StoreApproveStockTransferTest extends TestCase
                     Unit::identifier( $product->unit_quantities[0]->unit->identifier )->first()->id
                 );
             }
+        });
+        
+        $products->each( function( $product ) use ( $response, $quantityToTransfer ) {           
 
-            if ( ( float )( $product->newQuantity - $product->originalQuantity ) != ( float ) 2 ) {
+            if ( ( float )( $product->newQuantity - $product->originalQuantity ) != ( float ) $quantityToTransfer ) {
                 throw new Exception( sprintf( 
                     __( 'After transfect is approved, the stock has\'nt been updated : before => %s, after => %s, provided => %s, expected => %s'), 
                     $product->originalQuantity, 
                     $product->newQuantity,
-                    2,
-                    $product->originalQuantity + 2
+                    $quantityToTransfer,
+                    $product->originalQuantity + $quantityToTransfer
                 ) );
             }
         });
