1eada0c096f4d6559fa739804c08ef5f715b7103
Nejc Cotic <nejc.cotic@kitio.com>
2021-03-05 09:58:22 +0100

    adding schema support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -204,6 +204,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
         Schema::defaultStringLength(191);
         
         config([ 'nexopos.orders.statuses'      => [
