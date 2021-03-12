6f0036c40b21010299b7caf82939a60357359194
blair2004 <carlosjohnsonluv2004@gmail.com>
2021-03-12 23:01:39 +0100

    Update

--- resources/views/pages/setup/welcome.blade.php
+++ resources/views/pages/setup/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends( 'layout.base' )
 
 @section( 'layout.base.body' )
-<div id="nexopos-setup" class="h-full w-full flex items-center overflow-y-auto pb-10 bg-gray-300">
+<div id="nexopos-setup" class="h-full w-full items-center overflow-y-auto pb-10 bg-gray-300">
     <div class="container mx-auto p-4 md:p-0 flex-auto items-center justify-center flex">
         <div id="setup" class="w-full md:w-3/5 lg:w-2/5">
-            <div class="flex justify-center items-center py-6">
+            <div class="flex flex-shrink-0 justify-center items-center py-6">
                 <h2 class="text-6xl font-bold text-transparent bg-clip-text from-blue-400 to-purple-400 bg-gradient-to-br">NexoPOS</h2>
             </div>
             <router-view></router-view>
