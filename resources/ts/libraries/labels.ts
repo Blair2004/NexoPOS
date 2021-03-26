7928e0d9cdff6e7ebbd05cab944186ffeda40af5
blair2004 <carlosjohnsonluv2004@gmail.com>
2021-03-26 18:48:38 +0100

    Update - Fixed : Unknown process status. - Added : localization support on labels.ts

--- resources/ts/libraries/labels.ts
+++ resources/ts/libraries/labels.ts
@@ -1,40 +1,42 @@
+import { __ } from "./lang";
+
 export default class Labels {
     getTypeLabel( label ) {
         switch( label ) {
             // @todo localization needed here
-            case 'delivery' : return 'Delivery'; break;
-            case 'takeaway' : return 'Take Away'; break;
-            default : return 'Unknown Type'; break;
+            case 'delivery' : return __( 'Delivery' ); break;
+            case 'takeaway' : return __( 'Take Away' ); break;
+            default : return __( 'Unknown Type' ); break;
         }
     }
     getDeliveryStatus( label ) {
         switch( label ) {
             // @todo localization needed here
-            case 'pending' : return 'Pending'; break;
-            case 'ongoing' : return 'Ongoing'; break;
-            case 'delivered' : return 'Delivered'; break;
-            case 'failed' : return 'Delivery Failure'; break;
-            default : return 'Unknown Status'; break;
+            case 'pending' : return __( 'Pending' ); break;
+            case 'ongoing' : return __( 'Ongoing' ); break;
+            case 'delivered' : return __( 'Delivered' ); break;
+            case 'failed' : return __( 'Delivery Failure' ); break;
+            default : return __( 'Unknown Status' ); break;
         }
     }
     getProcessingStatus( label ) {
         switch( label ) {
             // @todo localization needed here
-            case 'pending' : return 'Pending'; break;
-            case 'ongoing' : return 'Ongoing'; break;
-            case 'done' : return 'Done'; break;
-            case 'failed' : return 'Failure'; break;
-            default : return 'Unknown Status'; break;
+            case 'pending' : return __( 'Pending' ); break;
+            case 'ongoing' : return __( 'Ongoing' ); break;
+            case 'ready' : return __( 'Ready' ); break;
+            case 'failed' : return __( 'Failure' ); break;
+            default : return __( 'Unknown Status' ); break;
         }
     }
     getPaymentStatus( label ) {
         switch( label ) {
             // @todo localization needed here
-            case 'paid' : return 'Paid'; break;
-            case 'hold' : return 'Hold'; break;
-            case 'unpaid' : return 'Unpaid'; break;
-            case 'partially_paid' : return 'Partially Paid'; break;
-            default : return 'Unknown Status'; break;
+            case 'paid' : return __( 'Paid' ); break;
+            case 'hold' : return __( 'Hold' ); break;
+            case 'unpaid' : return __( 'Unpaid' ); break;
+            case 'partially_paid' : return __( 'Partially Paid' ); break;
+            default : return __( 'Unknown Status' ); break;
         }
     }
 }
