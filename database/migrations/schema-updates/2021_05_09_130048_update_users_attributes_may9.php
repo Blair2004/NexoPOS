76f627733f7fa607bb97cbf61759aceabd254b7f
blair2004 <carlosjohnsonluv2004@gmail.com>
2021-05-24 11:16:09 +0100

    Update - Fixed : wrong class assigned to system migration.

--- database/migrations/schema-updates/2021_05_09_130048_update_users_attributes_may9.php
+++ database/migrations/schema-updates/2021_05_09_130048_update_users_attributes_may9.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Classes\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class UpdateUsersAttributesMay9 extends Migration
 {
