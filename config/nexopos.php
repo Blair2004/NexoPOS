a13d27bce2ba3e3f100b9537a94ccfa7e76b3962
Blair Jersyer <Blair2004@users.noreply.github.com>
2021-09-11 17:23:33 +0100

    Update nexopos.php

--- config/nexopos.php
+++ config/nexopos.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version'       =>  '4.5.1',
+    'version'       =>  '4.5.2',
     'languages'     =>  [
         'en'        =>  'English',
         'fr'        =>  'Français',
