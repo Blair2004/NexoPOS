62d2112d50be349ea3c654a1db64d4d7c569c132
blair2004 <carlosjohnsonluv2004@gmail.com>
2021-09-18 10:56:33 +0000

    Add snapshot for backing up the database

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'snapshots' =>  [
+            'driver'    =>  'local',
+            'root'      =>  storage_path( 'snapshots' )
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
