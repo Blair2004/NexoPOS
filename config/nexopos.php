76d8840eceda6900e990313cddc9fe0dc51f29ea
blair2004 <carlosjohnsonluv2004@gmail.com>
2021-11-09 11:39:44 +0000

    update version

--- config/nexopos.php
+++ config/nexopos.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version'       =>  '4.6.1',
+    'version'       =>  '4.6.2',
     'languages'     =>  [
         'en'        =>  'English',
         'fr'        =>  'Français',
