be3bf77466dc0875452c0d33fb4df44866cc08e1
blair2004 <carlosjohnsonluv2004@gmail.com>
2022-01-22 11:53:26 +0100

    Build

--- tests/Feature/CreateOrderTest.php
+++ tests/Feature/CreateOrderTest.php
@@ -2,24 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Classes\Currency;
-use App\Models\AccountType;
-use App\Models\CashFlow;
-use App\Models\Customer;
-use App\Models\CustomerCoupon;
-use App\Models\OrderPayment;
-use App\Models\OrderProductRefund;
-use App\Models\Product;
-use App\Models\Role;
-use App\Models\User;
-use App\Services\CurrencyService;
-use App\Services\TaxService;
-use Exception;
-use Laravel\Sanctum\Sanctum;
-use Illuminate\Support\Arr;
 use Tests\TestCase;
-use Faker\Factory;
-use Illuminate\Support\Facades\Event;
 use Tests\Traits\WithAuthentication;
 use Tests\Traits\WithOrderTest;
 
