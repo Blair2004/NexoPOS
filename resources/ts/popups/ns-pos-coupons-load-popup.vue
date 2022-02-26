6d7912721a86bc75cc5c074ed73d2a87c829d30e
blair2004 <carlosjohnsonluv2004@gmail.com>
2022-02-26 08:10:43 +0100

    Update

--- resources/ts/popups/ns-pos-coupons-load-popup.vue
+++ resources/ts/popups/ns-pos-coupons-load-popup.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="shadow-lg ns-box w-95vw md:w-3/6-screen lg:w-2/6-screen">
         <div class="border-b ns-box-header p-2 flex justify-between items-center">
-            <h3 class="font-bold ">{{ __( 'Load Coupon' ) }}</h3>
+            <h3 class="font-bold">{{ __( 'Load Coupon' ) }}</h3>
             <div>
                 <ns-close-button @click="closePopup()"></ns-close-button>
             </div>
