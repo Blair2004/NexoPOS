926e8da8ccfa551ea9662fdbfe55780d87a474fe
blair2004 <carlosjohnsonluv2004@gmail.com>
2022-08-25 16:58:01 +0100

    Update

--- app/Providers/ModulesServiceProvider.php
+++ app/Providers/ModulesServiceProvider.php
@@ -21,14 +21,6 @@ class ModulesServiceProvider extends ServiceProvider
     public function boot( ModulesService $modules )
     {
         /**
-         * we might sometime needs to
-         * ignore all module.
-         */
-        if ( ! env( 'NS_LOAD_MODULES', true ) ) {
-            return;
-        }
-        
-        /**
          * trigger boot method only for enabled modules
          * service providers that extends ModulesServiceProvider.
          */
@@ -52,14 +44,6 @@ class ModulesServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        /**
-         * we might sometime needs to
-         * ignore all module.
-         */
-        if ( ! env( 'NS_LOAD_MODULES', true ) ) {
-            return;
-        }
-
         $this->app->singleton( ModulesService::class, function( $app ) {
             $this->modules = new ModulesService;
             $this->modules->load();
