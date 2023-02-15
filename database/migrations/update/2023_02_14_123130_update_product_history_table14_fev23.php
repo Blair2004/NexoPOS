cabb44da414c2b9b0a4f2d1d5fa9f8eeb48af570
blair2004 <carlosjohnsonluv2004@gmail.com>
2023-02-15 02:48:43 +0100

    Remove Unused Class

--- database/migrations/update/2023_02_14_123130_update_product_history_table14_fev23.php
+++ database/migrations/update/2023_02_14_123130_update_product_history_table14_fev23.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Migration as ModelsMigration;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
