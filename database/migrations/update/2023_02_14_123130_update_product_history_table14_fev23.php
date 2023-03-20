138e5afe137916085ca6430f415e835aa3f98dc1
blair2004 <carlosjohnsonluv2004@gmail.com>
2023-03-20 19:18:38 +0100

    Merge d098046c4c40e7a1b143760b7a41455e98cf40be

--- database/migrations/update/2023_02_14_123130_update_product_history_table14_fev23.php
+++ database/migrations/update/2023_02_14_123130_update_product_history_table14_fev23.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Migration as ModelsMigration;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,8 @@ return new class extends Migration
                 $table->integer( 'order_product_id' )->nullable();
             }
         });
+
+        ModelsMigration::truncate();
     }
 
     /**
