ddfdd7ee7da08d50fd6b4dd8f80f0bd4bfbcc168
Stefan Wurzinger <swurzinger@gmx.at>
2023-05-04 00:29:50 +0200

    prevent double-closing popup, fix possible loop

--- resources/ts/libraries/popup-resolver.ts
+++ resources/ts/libraries/popup-resolver.ts
@@ -3,11 +3,12 @@
  * we need to resolve something when the popup
  * is being closed. Otherwise the popup is only closed
  * @param state
+ * @param closeImmediately
  */
-export default function( state: any ) {
+export default function( state: any, closeImmediately: boolean = false ) {
     if ( this.popup.params.resolve !== undefined && this.popup.params.reject ) {
         state !== false ? this.popup.params.resolve( state ) : this.popup.params.reject( state );
     }
-    
-    this.popup.close();
+
+    this.popup.close( null, closeImmediately );
 }
