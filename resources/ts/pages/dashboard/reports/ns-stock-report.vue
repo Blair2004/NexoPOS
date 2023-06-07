ab0a9c660b66cb8b723389907b2b88a6f1a71440
Moaaz Sobhy <moaaz.briek97@gmail.com>
2023-06-07 14:14:47 +0300

    Update ns-stock-report.vue

--- resources/ts/pages/dashboard/reports/ns-stock-report.vue
+++ resources/ts/pages/dashboard/reports/ns-stock-report.vue
@@ -49,7 +49,11 @@ export default {
     },
     methods: {
         printSaleReport() {
+          if (window.ns.language === 'ar') {
+            this.$htmlToPaper( 'sale-report' , {rtl: true});
+          } else {
             this.$htmlToPaper( 'sale-report' );
+          }
         },
         setStartDate( moment ) {
             this.startDate  =   moment.format();
