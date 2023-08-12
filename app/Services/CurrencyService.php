08927714926840e02f3e5294dbc841fb1c866f7f
blair2004 <carlosjohnsonluv2004@gmail.com>
2023-08-12 23:41:23 +0100

    Update - Fixed: tax computation

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -224,10 +224,13 @@ class CurrencyService
      */
     public function getRaw( $value = null )
     {
-        $value = $value !== null ? (string) BigDecimal::of( $value )->dividedBy(1, $this->decimal_precision, RoundingMode::UP ) : null;
-        $main = $value === null ? (string) $this->value->dividedBy(1, $this->decimal_precision, RoundingMode::UP ) : 0;
-
-        return floatval( $value !== null ? $value : $main );
+        if ( $value === null ) {
+            return $this->value->dividedBy( 1, $this->decimal_precision, RoundingMode::HALF_UP )->toFloat();
+        } else {
+            return BigDecimal::of( $value )->dividedBy( 1, $this->decimal_precision, RoundingMode::HALF_UP )->toFloat();
+        }
+        
+        return 0;
     }
 
     /**
