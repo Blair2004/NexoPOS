7e63c0568552412d21206a760fc53016ebfdd778
Blair2004 <carlosjohnsonluv2004@gmail.com>
2024-01-21 20:59:27 +0000

    Update - Added: Ensure set action perform a correct increase/decrease action - Updated: set is the default adjustment action by default - Removed: unused js files - Fixed: Field was disabled when the reset confirmation was closed

--- resources/views/pages/dashboard/settings/reset-footer.blade.php
+++ resources/views/pages/dashboard/settings/reset-footer.blade.php
@@ -4,7 +4,7 @@
             return ( form ) => new Promise( ( resolve, reject ) => {
                 Popup.show( nsConfirmPopup, {
                     title: __( 'Confirm Your Action' ),
-                    message: __( 'The database will be cleared. You\'ll loose existing datas. Only users and roles are kept. Would you like to proceed ?' ),
+                    message: __( 'The database will be cleared and all data erased. Only users and roles are kept. Would you like to proceed ?' ),
                     onAction: ( action ) => {
                         if ( action ) {
                             nsHttpClient.post( `/api/reset`, form.reset )
@@ -12,6 +12,8 @@
                                     next: resolve,
                                     error: reject
                                 })
+                        } else {
+                            reject( false );
                         }
                     }
                 })
