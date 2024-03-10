a91defc753b27e5eec2091dfd7eb8e82f746556b
blair2004 <carlosjohnsonluv2004@gmail.com>
2024-03-10 08:24:20 +0100

    build assets

--- public/build/assets/auth-D9WUaniz.js
+++
@@ -1,7 +0,0 @@
-import{_ as o}from"./preload-helper-BQ24v_F8.js";import{c as m}from"./bootstrap-BYakqcOB.js";import{b as n}from"./components-DsDPveR4.js";import{d as t}from"./runtime-core.esm-bundler-RT2b-_3S.js";import"./currency-lOMYG1Wf.js";import"./ns-alert-popup-SVrn5Xft.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./ns-avatar-image-CAD6xUGA.js";import"./index.es-Br67aBEV.js";import"./ns-prompt-popup-hggAl9jS.js";nsExtraComponents.nsRegister=t(()=>o(()=>import("./ns-register-BNrf_Cbt.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url));nsExtraComponents.nsLogin=t(()=>o(()=>import("./ns-login-Csw8UGc0.js"),__vite__mapDeps([5,1,2,3,4]),import.meta.url));nsExtraComponents.nsPasswordLost=t(()=>o(()=>import("./ns-password-lost-CBAVtaSb.js"),__vite__mapDeps([6,2,1,3,4]),import.meta.url));nsExtraComponents.nsNewPassword=t(()=>o(()=>import("./ns-new-password-CPOqWa1a.js"),__vite__mapDeps([7,2,1,3,4]),import.meta.url));window.nsHttpClient=nsHttpClient;window.authVueComponent=m({components:{...nsExtraComponents,...n}});for(let r in n)window.authVueComponent.component(r,n[r]);window.authVueComponent.mount("#page-container");
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./ns-register-BNrf_Cbt.js","./bootstrap-BYakqcOB.js","./currency-lOMYG1Wf.js","./runtime-core.esm-bundler-RT2b-_3S.js","./_plugin-vue_export-helper-DlAUqK2U.js","./ns-login-Csw8UGc0.js","./ns-password-lost-CBAVtaSb.js","./ns-new-password-CPOqWa1a.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- public/build/assets/setup-D4J4P3GB.js
+++
@@ -1,7 +0,0 @@
-import{_ as o}from"./preload-helper-BQ24v_F8.js";import{b as e}from"./components-DsDPveR4.js";import{c as p,a as n}from"./vue-router-bhgvo0O9.js";import{c as i}from"./bootstrap-BYakqcOB.js";import"./ns-alert-popup-SVrn5Xft.js";import"./currency-lOMYG1Wf.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./runtime-core.esm-bundler-RT2b-_3S.js";import"./ns-avatar-image-CAD6xUGA.js";import"./index.es-Br67aBEV.js";import"./ns-prompt-popup-hggAl9jS.js";const a=()=>o(()=>import("./welcome-DhnKJMzx.js"),__vite__mapDeps([0,1,2,3]),import.meta.url),s=()=>o(()=>import("./database-_R8tQYPW.js"),__vite__mapDeps([4,5,1,3,2]),import.meta.url),_=()=>o(()=>import("./setup-configuration-C1R_KeY6.js"),__vite__mapDeps([6,5,1,3,2]),import.meta.url),c=[{path:"/",component:a},{path:"/database",component:s},{path:"/configuration",component:_}],m=p({routes:c,history:n()}),t=i({});t.use(m);for(let r in e)t.component(r,e[r]);t.mount("#nexopos-setup");window.nsRouter=m;
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./welcome-DhnKJMzx.js","./currency-lOMYG1Wf.js","./_plugin-vue_export-helper-DlAUqK2U.js","./runtime-core.esm-bundler-RT2b-_3S.js","./database-_R8tQYPW.js","./bootstrap-BYakqcOB.js","./setup-configuration-C1R_KeY6.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
