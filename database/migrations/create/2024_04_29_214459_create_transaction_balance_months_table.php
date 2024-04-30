85f49c8688f5ad0a4b3c1b7f8a0f4255a0589cb1
blair2004 <carlosjohnsonluv2004@gmail.com>
2024-04-30 12:48:27 +0100

    Update

---
+++ database/migrations/create/2024_04_29_214459_create_transaction_balance_months_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nexopos_transactions_balance_months', function (Blueprint $table) {
+            $table->id();
+            $table->float( 'opening_balance' )->default(0);
+            $table->float( 'income' )->default(0);
+            $table->float( 'expense' )->default(0);
+            $table->float( 'closing_balance' )->default(0);
+            $table->date( 'date' )->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nexopos_transactions_balance_months');
+    }
+};
