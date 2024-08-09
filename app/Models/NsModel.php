3276edc973358b7300213e493f893e9201aba34f
blair2004 <carlosjohnsonluv2004@gmail.com>
2024-08-09 15:27:55 +0100

    WIP

--- app/Models/NsModel.php
+++ app/Models/NsModel.php
@@ -25,4 +25,64 @@ abstract class NsModel extends NsRootModel
 
         $this->table = Hook::filter( 'ns-model-table', $this->table );
     }
+
+    protected $dispatchableFieldsEvents     =   [];
+
+    /**
+     * We would like to be able to monitor
+     * accurately all changes that occurs on a model
+     */
+    protected $oldAttributes = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->oldAttributes = $model->getOriginal();
+        });
+
+        static::updating(function ($model) {
+            $model->oldAttributes = $model->getOriginal();
+        });
+
+        static::created(function ($model) {
+            $model->detectChanges();
+        });
+
+        static::updated(function ($model) {
+            $model->detectChanges();
+        });
+    }
+
+    protected function detectChanges()
+    {
+        $changedAttributes = array_diff_assoc($this->getAttributes(), $this->oldAttributes);
+
+        if (!empty($changedAttributes)) {
+            // Dispatch the "changed" event for the entire model
+            $this->fireModelEvent('changed', false);
+
+            // Check for specific field changes and dispatch events accordingly
+            foreach ($this->dispatchableFieldsEvents as $field => $class) {
+                if (array_key_exists($field, $changedAttributes)) {
+                    event( new $class($this, $this->oldAttributes[$field] ?? null, $this->getAttribute($field)));
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the event map for the model.
+     *
+     * @return array
+     */
+    public function getChangedEventPayload()
+    {
+        return [
+            'old' => $this->oldAttributes,
+            'new' => $this->getAttributes(),
+            'changed' => array_keys(array_diff_assoc($this->getAttributes(), $this->oldAttributes)),
+        ];
+    }
 }
