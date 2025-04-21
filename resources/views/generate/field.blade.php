890a6678e697c3bbddbf8dd91c054e662d772281
blair2004 <carlosjohnsonluv2004@gmail.com>
2025-04-21 20:51:50 +0000

    WIP

--- resources/views/generate/field.blade.php
+++ resources/views/generate/field.blade.php
@@ -6,14 +6,7 @@ use App\Services\FieldsService;
 
 class DummyClass extends FieldsService
 {
-    /**
-     * The unique identifier of the class.
-     */
     const IDENTIFIER = 'DummyIdentifier';
-
-    /**
-     * Defines wether the class should be automatically loaded.
-     */
     const AUTOLOAD = false;
 
     public function get()
