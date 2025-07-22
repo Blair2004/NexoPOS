e88aefa411f015d16a6cb5f6420767c02fa8aaef
blair2004 <carlosjohnsonluv2004@gmail.com>
2025-07-22 23:20:25 +0000

    WIP

--- database/permissions/pos.php
+++ database/permissions/pos.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Permission;
+use App\Models\Role;
 
 if ( ! Permission::namespace( 'nexopos.pos.edit-purchase-price' ) instanceof Permission ) {
     $pos = Permission::firstOrNew( [ 'namespace' => 'nexopos.pos.edit-purchase-price' ] );
@@ -40,4 +41,37 @@ if ( ! Permission::namespace( 'nexopos.pos.delete-order-product' ) instanceof Pe
     $pos->namespace = 'nexopos.pos.delete-order-product';
     $pos->description = __( 'Let the user delete order products on POS.' );
     $pos->save();
+}
+
+// Create POS cart action permissions
+$posCartPermissions = [
+    'nexopos.cart.product-discount' => __( 'Cart: Change Product Discount' ),
+    'nexopos.cart.product-price' => __( 'Cart: Edit Product Price' ),
+    'nexopos.cart.product-wholesale-price' => __( 'Cart: Use Wholesale Price' ),
+    'nexopos.cart.product-delete' => __( 'Cart: Product Delete' ),
+    'nexopos.cart.settings' => __( 'Cart: Change Settings' ),
+    'nexopos.cart.taxes' => __( 'Cart: Set Taxes' ),
+    'nexopos.cart.comments' => __( 'Cart: Add Comments' ),
+    'nexopos.cart.order-type' => __( 'Cart: Change Order Type' ),
+    'nexopos.cart.coupons' => __( 'Cart: Apply Coupons' ),
+    'nexopos.cart.products' => __( 'Cart: Create Quick Product' ),
+    'nexopos.cart.void' => __( 'Cart: Void Order' ),
+    'nexopos.cart.discount' => __( 'Cart: Apply Discount' ),
+    'nexopos.cart.hold' => __( 'Cart: Hold Order' ),
+];
+
+foreach ( $posCartPermissions as $namespace => $name ) {
+    $permission = Permission::firstOrNew( [ 'namespace' => $namespace ] );
+    $permission->name = $name;
+    $permission->namespace = $namespace;
+    $permission->description = sprintf( __( 'Allow access to %s feature in POS cart.' ), strtolower( $name ) );
+    $permission->save();
+}
+
+/**
+ * Assign new permissions to admin role
+ */
+$admin = Role::namespace( 'admin' );
+if ( $admin instanceof Role ) {
+    $admin->addPermissions( array_keys( $posCartPermissions ) );
 }
