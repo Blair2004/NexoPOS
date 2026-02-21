53625174f6dc1864df3243eca08168d2f514396c
Ayoba-Tech <129840789+Crooks39822@users.noreply.github.com>
2026-02-21 14:54:30 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="ns-box rounded shadow w-full md:w-2/5 lg:w-2/5 overflow-hidden">
                 <div id="section-header" class="ns-box-header p-4">
-                    <p class="text-center b-8 text-sm">{{ __( "If you see this page, this means NexoPOS is correctly installed on your system. As thippps page is meant to be the frontend, NexoPOS doesn't have a frontend for the meantime. This page shows useful links that will take you to the important resources." ) }}</p>
+                    <p class="text-center b-8 text-sm">{{ __( "If you see this page, this means NexoPOS is correctly installed on your system. As this page is meant to be the frontend, NexoPOS doesn't have a frontend for the meantime. This page shows useful links that will take you to the important resources." ) }}</p>
                 </div>
                 <div class="ns-box-footer flex shadow border-t">
                     <div class="flex w-1/3"><a class="link text-sm w-full py-2 text-center" href="{{ ns()->route( 'ns.dashboard.home' ) }}">{{ __( 'Dashboard' ) }}</a></div>
